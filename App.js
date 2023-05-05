ebf8a83fa756762e8bee038bc1ae8c805e7da820
Ak-Rakib <rkrakibsaimon@gmail.com>
2023-05-05 20:51:46 +0600

    set chefs id

--- App.js
+++ App.js
@@ -7,10 +7,22 @@ app.use(cors());
 
 const chefInfo = require('./Data/chef.json');
 const customerReview = require('./Data/customer.json');
+const allRecipes = require('./Data/recipes.json');
 
 
 app.get('/chefs', (req, res) => {
     res.send(chefInfo);
+});
+
+app.get('/chefs/:id', (req, res) => {
+    const id = req.params.id;
+    console.log(id);
+    const specificChef = chefInfo.find(c => c.id === id);
+    res.send(specificChef);
+});
+
+app.get('/recipes', (req, res) => {
+    res.send(allRecipes);
 });
 
 app.get('/reviews', (req, res) => {
